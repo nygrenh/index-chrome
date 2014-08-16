a27470e375e381b431b690e8c3a72ecc8e1d5532
Henrik Nygren <nygrenh@gmail.com>
2014-08-16 20:27:25 +0300

    Fill title and url automatically

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ var indexUrl = "https://theindex.herokuapp.com/";
 var frame = document.getElementById("frame");
 var loadHandler = function() {
   frame.removeEventListener('load', loadHandler);
-  frame.src = indexUrl + "links/new";
+  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+    var tab = tabs[0];
+    indexUrl += "links/new?url=";
+    indexUrl += encodeURIComponent(tab.url);
+    indexUrl += "&title=" + encodeURIComponent(tab.title);
+    frame.src = indexUrl;
+  });
 };
 frame.addEventListener('load', loadHandler);
